635139f6db4cc9320b9bbf5a810a96417577745a
Nirmalya Ghosh <nirmalya.email@gmail.com>
2022-04-18 00:14:24 +0530

    feat: adds Keystone

--- next.config.js
+++ next.config.js
@@ -3,28 +3,31 @@
  */
 
 const { withContentlayer } = require("next-contentlayer");
+const { withKeystone } = require("@keystone-6/core/next");
 
-module.exports = withContentlayer({
-  reactStrictMode: true,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom": "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-      });
-    }
+module.exports = withKeystone(
+  withContentlayer({
+    reactStrictMode: true,
+    webpack: (config, { dev, isServer }) => {
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+          react: "preact/compat",
+          "react-dom": "preact/compat",
+          "react-dom/test-utils": "preact/test-utils",
+        });
+      }
 
-    return config;
-  },
-  async redirects() {
-    return [
-      {
-        source: "/concepts/:slug",
-        destination: "/articles/:slug",
-        permanent: true,
-      },
-    ];
-  },
-});
+      return config;
+    },
+    async redirects() {
+      return [
+        {
+          source: "/concepts/:slug",
+          destination: "/articles/:slug",
+          permanent: true,
+        },
+      ];
+    },
+  })
+);
